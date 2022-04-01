dc0beeb67eaeca0c25bcf3840aa792419b607814
nllii <ch1ris23@ymail.com>
2022-04-01 17:00:30 -0500

    updating

--- filterMenu.py
+++ filterMenu.py
@@ -34,7 +34,7 @@ def jobfilter_menu(jobsListing):
             writer = csv.writer(csv_file)
             writer.writerow(["Not_Interested",jobsListing[0], jobsListing[1], jobsListing[2], jobsListing[3]])
     
-    def Justsave(jobsListing):
+    def option4(jobsListing):
         with open('progress_filter.csv', 'a') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(["save",jobsListing[0], jobsListing[1], jobsListing[2], jobsListing[3]])
@@ -43,19 +43,23 @@ def jobfilter_menu(jobsListing):
     print_menu()
     option = ''
     try:
-        option = int(input('Enter your choice: '+ "\n"))
+        option = int(input('Enter your choice: '))
     except:
-        Justsave(jobsListing)
+        option4(jobsListing)
         # print('Wrong input. Please enter a number ...')
     #Check what choice was entered and act accordingly
-    if option == 1:    
+    if option == 1:
+        print("Applied")
         option1(jobsListing)
     elif option == 2:
+        print("Not Applied")
         option2(jobsListing)
     elif option == 3:
+        print("Not Interested")
         option3(jobsListing)
     elif option == 4:
-        Justsave(jobsListing)
+        print("Just continue...")
+        option4(jobsListing)
     #     exit()
     # else:
     #     print('Invalid option. Please enter a number between 1 and 4.')
